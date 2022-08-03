c4300eabe69c8b3cf9ef281f60848fc5b7a24fba
Israel Afangideh <105407225+IsraelAfangideh@users.noreply.github.com>
2022-08-03 00:54:31 -0500

    New Database structure, started building in Spring
    andikappa(akpan).sql is the sql script for database

---
+++ andikappa/andikappa/src/main/java/com/AfangidehAyah/models/x.java
@@ -0,0 +1,27 @@
+package com.AfangidehAyah.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class x {
+	
+	private int x_id;
+	private String english_word;
+	private String efik_word;
+	private String word_type;
+	private String context;
+	
+	
+	//boilerplate code
+	
+	public x() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ andikappa/andikappa/src/test/java/com/AfangidehAyah/AndikappaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AfangidehAyah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndikappaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
