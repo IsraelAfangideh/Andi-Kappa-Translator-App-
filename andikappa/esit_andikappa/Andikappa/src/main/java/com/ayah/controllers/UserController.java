4c8ef76c32042bb567ce2575c277c07e948ecb1b
IsraelAfangideh <israelafangideh@gmail.com>
2022-09-14 12:58:56 -0500

    Register User functionality completed

--- andikappa/esit_andikappa/Andikappa/src/main/java/com/ayah/controllers/UserController.java
+++ andikappa/esit_andikappa/Andikappa/src/main/java/com/ayah/controllers/UserController.java
@@ -1,23 +1,43 @@
 package com.ayah.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ayah.daos.UserDAO;
+import com.ayah.models.Users;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/agwo", produces = "application/json")
 @CrossOrigin
 public class UserController {
-    private UserDAO uDAO;
+    private UserDAO ud;
 
     @Autowired
     public UserController(UserDAO ud) {
 		super();
-		this.uDAO = ud;
+		this.ud = ud;
 	}
 
+    @PostMapping(value = "/register")
+    public ResponseEntity addUser(@RequestBody Users u){
+
+        Users newUser = ud.save(u);
+        System.out.println(newUser);
+        System.out.println(newUser);
+        
+        if(newUser == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.accepted().body("Welcome:" + u);
+    }
+
+
     
+
 }
