03bbf40bc75e16ffe0fac26b60c2b4edb5e93120
Afangideh <2234381@cognizant.com>
2022-12-12 07:50:10 -0600

    minimized the code

---
+++ andikappa/iso_andikappa/add.js
@@ -0,0 +1,112 @@
+// CONSTANTS
+
+const url = "http://localhost:7777/"
+// const propernoun = document.getElementById("wordtype-").innerText
+
+// FUNCTION CALLS
+  document.getElementById("submitNewTranslation").onclick = addtranslation
+  // document.getElementById("language").onclick = languagecheck
+  document.getElementById("ö").onclick = addö
+
+
+// document.getElementById("englishword").onchange = convertcase
+//   document.getElementById("efikword").onchange = convertcase
+//   document.getElementById("wordtype").onchange = convertcase
+//    document.getElementById("context").onchange = convertcase
+
+
+
+  // IN LINE FUNCTIONS
+  // window.onload = function() {
+  //   document.getElementById("language").value = "Add New Translation"
+  // }
+
+   document.getElementById("ñ").addEventListener ('click', function() { 
+    document.getElementById("efikword").value = document.getElementById("efikword").value += "ñ"})
+
+  
+
+      //DIVORCED FUNCTIONS
+   function addñ(){
+
+    document.getElementById("efikword").value = document.getElementById("efikword").value += "ñ"
+
+   }
+
+   function addö(){
+ document.getElementById("efikword").value = document.getElementById("efikword").value += "ö"
+}
+   
+
+
+//   function languagecheck (){
+//     console.log(document.getElementById("language").value)
+
+// if (document.getElementById("language").value == "Search English Word") {
+
+//     console.log("englishdetected")
+//     window.location.href = "getEnglish.html"
+
+
+    
+// }else if (document.getElementById("language").value == "Search Efik Word") {
+
+   
+//     window.location.href = "getEfik.html"
+
+    
+
+// }
+// }
+
+
+
+// function convertcase(){
+//     console.log(propernoun)
+//     document.getElementById("englishword").textContent.toLowerCase
+//     if (propernoun == "Proper Noun"){
+//         console.log("is proper noun")
+//        document.getElementById("englishword").textContent.toLowerCase
+//     }else if (propernoun != "Proper Noun"){
+//         console.log("is NOT proper noun")
+//         document.getElementById("englishword").textContent.toUpperCase   
+//     }
+//     }
+async function addtranslation () {
+
+
+let translationjson = {
+    "englishword": document.getElementById("englishword").value,
+    "efikword": document.getElementById("efikword").value,
+    "context": document.getElementById("context").value
+
+}
+console.log(translationjson)
+let tss = JSON.stringify(translationjson)
+
+console.log(translationjson)
+    let response = await fetch(url + "add",{
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-16"},
+        body: tss,
+        credentials: "same-origin"
+    })
+
+
+console.log(response.body)
+
+
+
+if (response.status == 202){
+   
+   document.getElementById("directiveh1").innerHTML = ("You have added the following translation:" + tss)
+
+   
+}
+else{
+document.getElementById("directiveh1").innerText = "unsuccessful, try again"
+}
+}
+
+ 
+ 
---
+++ andikappa/iso_andikappa/fetch.js
@@ -0,0 +1,48 @@
+const url = "http://localhost:7777/"
+
+// document.getElementById("language").onclick = languagecheck
+
+document.getElementById("submit").onclick = getefikword
+// document.getElementById("submit").onclick = getayeneefik
+// document.getElementById("submit").onclick = getayenecontext
+
+
+async function getefikword(){
+    
+    document.getElementById("tablebody").innerHTML = ""
+let efikword = document.getElementById("efikword").value
+// console.log("hii")
+    let response = await fetch (url + "fetch/" + efikword,{
+      
+    })
+    console.log(response.json)
+
+    if (response.status == 200){
+        let data = await response.json();
+
+        console.log(data)
+
+        for (let englishword of data){
+            let row = document.createElement("tr");
+      let cell = document.createElement("td");
+      cell.innerHTML = englishword.englishword
+
+      let cell2 = document.createElement("td")
+      cell2.innerHTML =englishword.wordtype
+
+      let cell3 = document.createElement("td")
+      cell3.innerHTML = englishword.context
+            row.appendChild(cell)
+      row.appendChild(cell2)
+      row.appendChild(cell3)
+
+      document.getElementById("tablebody").appendChild(row);
+
+
+        }
+    }else{
+        alert("Something went wrong, try again");
+    }
+
+}
+
