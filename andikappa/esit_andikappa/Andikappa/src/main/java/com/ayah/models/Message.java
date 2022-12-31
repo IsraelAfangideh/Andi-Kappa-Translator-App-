1f6e9088db0caec0a54ef175ed2bb0e0ed7fd84d
IsraelAfangideh <israelafangideh@gmail.com>
2022-12-31 11:09:14 -0600

    Made a model for messages

---
+++ andikappa/esit_andikappa/Andikappa/src/main/java/com/ayah/models/Message.java
@@ -0,0 +1,12 @@
+package com.ayah.models;
+
+public class Message {
+    //List of Users involved in the message
+
+
+    //Time(We will display messages ranked by time)
+
+    //String messagecontent
+
+
+}
